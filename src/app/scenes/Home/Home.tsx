e1a4f8bd2969f7bb5056b245be955831bd461dc2
Carlos Petit <carlos.petit@sr-consultores.com>
2020-06-25 18:46:17 -0400

    update home screen

--- src/app/scenes/Home/Home.tsx
+++ src/app/scenes/Home/Home.tsx
@@ -5,12 +5,15 @@ import { connect } from 'react-redux'
 
 import { HomeNavProps } from 'src/types/HomeParamaList'
 import { Center } from '../../components/Center'
-import { FlatList, Button, Platform } from 'react-native'
+import { FlatList, Button, Platform, View, Text } from 'react-native'
 import faker from "faker";
 import { getHomeItems } from '../../actions/HomeListAction'
 import Loading from '../Loading/Loading'
+import { Size } from '../../services/Service'
+import colors from '../../assets/Assets'
 
 
+colors
 
 interface HomeProps {
     navigation: any,
@@ -37,24 +40,29 @@ class Home extends React.Component<HomeProps, State> {
     render() {
         return (
             <Center>
-                {
-                    !this.props.home.isFetching &&
-                    <FlatList
-                        contentContainerStyle={{}}
-                        style={{ width: "100%" }}
-                        data={this.getData()}
-                        extraData={this.props}
-                        renderItem={({ item }) => {
-                            return (
-                                <Button title={item.name} onPress={() => {
-                                    this.props.navigation.navigate('Detail', { name: item.name })
-                                }
-                                } />
-                            )
-                        }}
-                        keyExtractor={(item, index) => item + index}
-                    />
-                }
+                <View style={{ flex: 1, justifyContent: 'center' }}>
+                    <Text style={{ fontSize: Size(56), fontFamily: 'BogleWeb-Bold' }}>Selecciona tu pedido</Text>
+                </View>
+                <View style={{ flex: 8 }}>
+                    {
+                        !this.props.home.isFetching &&
+                        <FlatList
+                            contentContainerStyle={{ flex: 0 }}
+                            style={{ width: "100%" }}
+                            data={this.getData()}
+                            extraData={this.props}
+                            renderItem={({ item }) => {
+                                return (
+                                    <View key={item.id}>
+                                        <Text>{item.name}</Text>
+                                    </View>
+                                )
+                            }}
+                            keyExtractor={(item, index) => item + index}
+                        />
+                    }
+                </View>
+
                 {
                     this.props.home.isFetching &&
                     <Loading />
@@ -76,3 +84,9 @@ const mapDispatchToProps = (dispatch: any) => ({
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
+
+
+ // <Button title={item.name} onPress={() => {
+                                //     this.props.navigation.navigate('Detail', { name: item.name })
+                                // }
+                                // } />
