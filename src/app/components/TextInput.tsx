fa5cd849ac3a4549bd3b3573e69ad1914cae6c57
Carlos Petit <carlos.petit@sr-consultores.com>
2020-06-24 21:13:21 -0400

    add custom textinput

--- src/app/components/TextInput.tsx
+++ src/app/components/TextInput.tsx
@@ -1,33 +1,50 @@
 import React from 'react'
-import { View, Text, Dimensions } from 'react-native';
+import { View, Text, Dimensions, TextInput, StyleSheet } from 'react-native';
 import { Size } from '../services/Service';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import colors from '../assets/Colors';
 
-interface TextInputProps {
+
+interface CustomInputProps {
     value: string,
-    onChangeText: (text: string) => any
+    onChangeText: (text: string) => any,
+    placeholder: string,
+    type: boolean
 }
 
-export const TextInput: React.FC<TextInputProps> = ( props ) => {
+export const CustomInput: React.FC<CustomInputProps> = (props) => {
     return (
-        <View style={
-            {
-                alignItems: 'flex-start',
-                paddingStart: Size(95),
-                width: Dimensions.get('screen').width - Size(95),
-                height: Size(117),
-                borderRadius: Size(27),
-                borderWidth: Size(1),
-                borderColor: "rgba(117, 117, 117, 255)",
-                justifyContent: 'center',
-                alignContent: 'center',
-                marginTop: Size(66)
-            }
-        } >
+        <View style={styles.intputContainer} >
+            <MaterialCommunityIcons name={props.type ? "lock" : "rectangle"} size={24} color={colors.lightgray} />
             <TextInput
-                style={{ width: Dimensions.get('screen').width - Size(95), height: Size(117), justifyContent: 'center', alignItems: 'center' }}
+                style={styles.InputTextStyle}
                 onChangeText={(text: string) => props.onChangeText(text)}
                 value={props.value}
-                placeholder="Ingresa Rut" />
+                placeholder={props.placeholder}
+                secureTextEntry={props.type} />
         </View>
     );
 }
+
+
+const styles = StyleSheet.create({
+    intputContainer: {
+        alignItems: 'center',
+        paddingStart: Size(95),
+        width: Dimensions.get('screen').width - Size(95),
+        height: Size(117),
+        borderRadius: Size(27),
+        borderWidth: Size(1),
+        borderColor: "rgba(117, 117, 117, 255)",
+        justifyContent: 'center',
+        alignContent: 'center',
+        marginTop: Size(66),
+        flexDirection: 'row'
+    },
+    InputTextStyle: {
+        width: Dimensions.get('screen').width - Size(95),
+        height: Size(117),
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
