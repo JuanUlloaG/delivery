b364c2e1e49fb5126b6ed16e519b287e587f0d5a
Carlos Petit <carlos.petit@sr-consultores.com>
2020-06-25 21:37:39 -0400

    add countdown view to orders

---
+++ src/app/components/CountDown.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import moment from 'moment';
+
+interface CountDownProps {
+    date: string,
+}
+
+
+export default function CountDown(props: CountDownProps) {
+    // let date = moment(props.date, "YYYY-MM-DD HH:mm:ss")
+    // let initMinute = date.minutes()
+    // let finishMinute = date.add(15, "minute").minute()
+    // let minute = (initMinute - initMinute)
+    // console.log(date.add(15, "minute").minute());
+    const [minutes, setMinutes] = useState(14)
+    const [seconds, setSeconds] = useState(59)
+
+    useEffect(() => {
+        let timer = 0
+        if (seconds > 0 && minutes >= 0) {
+            timer = setInterval(() => setSeconds(seconds - 1), 1000);
+        }
+        if (timer == 0 && minutes >= 0) {
+            setSeconds(60)
+            setMinutes(minutes - 1)
+        }
+
+        return () => clearInterval(timer);
+    }, [seconds]);
+
+    return (
+        <View>
+            <Text>{"Tiempo restante: " + minutes + " : " + seconds + "s"}</Text>
+        </View>
+    )
+}
