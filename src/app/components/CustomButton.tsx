0664c2f288a7e26118488b0c42199606ceecfbb8
Carlos Petit <carlos.petit@sr-consultores.com>
2020-06-26 22:50:50 -0400

    refactoring custoButton

--- src/app/components/CustomButton.tsx
+++ src/app/components/CustomButton.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { View, Text, Dimensions, TextInput, Picker, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Size } from '../services/Service';
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import colors from '../assets/Colors';
 import { RFValue } from "react-native-responsive-fontsize";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
@@ -13,29 +12,26 @@ interface CustomButtonProps {
 
 export const CustomButton: React.FC<CustomButtonProps> = (props) => {
     return (
-        <TouchableOpacity style={
-            {
-                marginTop: 36,
-                alignItems: 'center',
-                justifyContent: 'center',
-                width: wp(100) - Size(95),
-                height: hp(8),
-                borderRadius: Size(32),
-                backgroundColor: colors.lightBlue
-            }
-        } >
-            <Text onPress={() => { props.login() }} style={
-                {
-                    fontFamily: "AvenirNextBold",
-                    fontSize: RFValue(Size(56)),
-                    color: "rgba(0, 0, 0, 255)"
-                }
-            } > Iniciar sesión </Text>
+        <TouchableOpacity style={styles.buttonContainer}>
+            <Text onPress={() => { props.login() }} style={styles.buttonText}> Iniciar sesión </Text>
         </TouchableOpacity>
     );
 }
 
 
 const styles = StyleSheet.create({
-
+    buttonContainer: {
+        marginTop: 36,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: wp(100) - Size(95),
+        height: hp(8),
+        borderRadius: Size(32),
+        backgroundColor: colors.lightBlue
+    },
+    buttonText: {
+        fontFamily: "AvenirNextBold",
+        fontSize: RFValue(Size(56)),
+        color: "rgba(0, 0, 0, 255)"
+    }
 });
