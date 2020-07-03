adc060f36055ea4ceed6e92cc981da512d6b430e
Carlos Petit <carlos.petit@sr-consultores.com>
2020-07-02 22:45:17 -0400

    add route to delivery home

--- src/app/navigations/HomeStack.tsx
+++ src/app/navigations/HomeStack.tsx
@@ -5,6 +5,7 @@ import { Text, TouchableOpacity, FlatList, Button, View } from 'react-native';
 import { AuthContext } from '../providers/AuthProvider';
 import { HomeParamList, HomeNavProps } from '../types/HomeParamaList';
 import Home from '../scenes/Home/Home';
+import HomeAddres from '../scenes/Home/HomeAddres';
 import { DetailStack } from './DetailStack';
 import { Size } from '../services/Service';
 import colors from '../assets/Colors';
@@ -21,9 +22,9 @@ const Stack2 = createStackNavigator<HomeParamList>()
 function HomeNavigator({ navigation, route }) {
     const { logout, getProfile } = useContext(AuthContext)
     return (
-        <Stack2.Navigator initialRouteName="Home">
-            <Stack2.Screen name="Home" component={Home} options={(navigation) => ({
-                headerTitle: "Lista de pedidos",
+        <Stack2.Navigator initialRouteName="HomeAddres">
+            <Stack2.Screen name="HomeAddres" component={HomeAddres} options={(navigation) => ({
+                headerTitle: "Pedidos Pendientes de entrega",
                 headerStyle: {
                     backgroundColor: colors.darkBlue,
                 },
@@ -53,6 +54,37 @@ function HomeNavigator({ navigation, route }) {
                 // )
             })
             } />
+            {/* <Stack2.Screen name="Home" component={Home} options={(navigation) => ({
+                headerTitle: "Lista de pedidos",
+                headerStyle: {
+                    backgroundColor: colors.darkBlue,
+                },
+                headerTitleStyle: {
+                    textAlign: 'center',
+                    flexGrow: 1,
+                    alignSelf: 'center',
+                    color: colors.white,
+                    fontFamily: fonts.primaryFontTitle,
+                    fontSize: Size(77),
+                },
+                headerStatusBarHeight: Size(35)
+                // headerRight: () => (
+                //     <TouchableOpacity onPress={() => { null }}>
+                //         <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginRight: 15 }}>
+                //             <Icon name='location-on' size={24} color="tomato" />
+                //         </View>
+                //     </TouchableOpacity>
+                // ),
+                // headerLeft: () => (
+
+                //     <TouchableOpacity onPress={() => { logout() }}>
+                //         <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginLeft: 15 }}>
+                //             <Icon name='camera' size={24} color="tomato" />
+                //         </View>
+                //     </TouchableOpacity>
+                // )
+            })
+            } /> */}
         </Stack2.Navigator>
     )
 }
