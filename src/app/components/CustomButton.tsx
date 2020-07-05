4c99ac812afa8046477e192fbaf0d192ad8c8002
Carlos Petit <carlos.petit@sr-consultores.com>
2020-07-04 20:49:33 -0400

    update custom button

--- src/app/components/CustomButton.tsx
+++ src/app/components/CustomButton.tsx
@@ -5,15 +5,67 @@ import colors from '../assets/Colors';
 import { RFValue } from "react-native-responsive-fontsize";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
+type size = "xs" | "s" | "m" | "l"
 
 interface CustomButtonProps {
-    login: () => any
+    onPress: () => any,
+    disable?: boolean,
+    size?: size,
+    color?: string
+}
+
+const defaultProps: CustomButtonProps = {
+    onPress: () => { },
+    disable: false,
+    size: "m",
+    color: colors.lightBlue
 }
 
 export const CustomButton: React.FC<CustomButtonProps> = (props) => {
+    const propss = Object.assign({}, defaultProps, props);
+    let width
+    let height
+    let fontsize
+    switch (props.size) {
+        case "xs":
+            width = (wp(100) - Size(95)) / 7
+            height = hp(8) * 0.75
+            fontsize = Size(51)
+            break;
+        case "s":
+            width = (wp(100) - Size(95)) / 3
+            height = hp(8) * 0.80
+            fontsize = Size(51)
+            break;
+        case "m":
+            width = (wp(100) - Size(95)) / 2
+            height = hp(8) * 0.80
+            fontsize = Size(51)
+            break;
+        case "l":
+            width = wp(100) - Size(95)
+            height = hp(8)
+            break;
+
+        default:
+            width = 24
+            fontsize = Size(39)
+            break;
+    }
+
+    let buttonStyle = {
+        marginTop: 36,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: width,
+        height: height,
+        borderRadius: Size(32),
+        backgroundColor: propss.color
+    }
+
     return (
-        <TouchableOpacity style={styles.buttonContainer}>
-            <Text onPress={() => { props.login() }} style={styles.buttonText}> Iniciar sesión </Text>
+        <TouchableOpacity style={[styles.buttonContainer, buttonStyle]} disabled={propss.disable} onPress={() => { props.onPress() }}>
+            {props.children}
         </TouchableOpacity>
     );
 }
@@ -21,13 +73,13 @@ export const CustomButton: React.FC<CustomButtonProps> = (props) => {
 
 const styles = StyleSheet.create({
     buttonContainer: {
-        marginTop: 36,
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: wp(100) - Size(95),
-        height: hp(8),
-        borderRadius: Size(32),
-        backgroundColor: colors.lightBlue
+        // marginTop: 36,
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        // width: wp(100) - Size(95),
+        // height: hp(8),
+        // borderRadius: Size(32),
+        // backgroundColor: colors.lightBlue
     },
     buttonText: {
         fontFamily: "AvenirNextBold",
