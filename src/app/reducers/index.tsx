83bc79521a535386115cee98d332b7dc55a745aa
Carlos Petit <carlos.petit@sr-consultores.com>
2020-07-07 18:46:02 -0400

    update index reducer

--- src/app/reducers/index.tsx
+++ src/app/reducers/index.tsx
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from "./AuthReducer";
 import homeReducer from "./HomeReducer";
+import shopReducer from "./ShopsReducer";
 
 export default combineReducers({
     auth: authReducer,
     home: homeReducer,
+    shop: shopReducer,
 });
