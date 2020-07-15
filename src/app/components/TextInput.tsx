f6c190b1c70a6336274f57b47529eb7757d0c00b
Carlos Petit <carlos.petit@sr-consultores.com>
2020-07-15 15:50:40 -0400

    Update textInput

--- src/app/components/TextInput.tsx
+++ src/app/components/TextInput.tsx
@@ -7,6 +7,7 @@ import { RFValue } from "react-native-responsive-fontsize";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 type keyType = "phone-pad" | "default" | "numeric"
+type size = "xs" | "s" | "m" | "l"
 
 interface CustomInputProps {
     onChangeText: (text: string) => any,
@@ -16,7 +17,8 @@ interface CustomInputProps {
     type?: boolean,
     editable: boolean,
     icon?: boolean,
-    keyType?: keyType
+    keyType?: keyType,
+    size?: size
 }
 
 const defaultProps: CustomInputProps = {
@@ -27,20 +29,72 @@ const defaultProps: CustomInputProps = {
     type: false,
     editable: true,
     icon: false,
-    keyType: 'default'
+    keyType: 'default',
+    size: 'l'
 }
 
 export const CustomInput: React.FC<CustomInputProps> = (props) => {
     const propss = Object.assign({}, defaultProps, props);
+
+    let width = wp(100) - Size(75)
+    let height = Size(35)
+
+    let fontsize
+    switch (propss.size) {
+        case "xs":
+            width = wp(40) - Size(95)
+            height = Size(117)
+            fontsize = Size(51)
+            break;
+        case "s":
+            width = wp(60) - Size(95)
+            height = Size(117)
+            fontsize = Size(51)
+            break;
+        case "m":
+            width = wp(80) - Size(95)
+            height = Size(117)
+            fontsize = Size(51)
+            break;
+        case "l":
+            width = wp(100) - Size(95)
+            height = Size(117)
+            break;
+
+        default:
+            width = wp(100) - Size(95)
+            height = Size(117)
+            break;
+    }
+
+    let intputContainerCustom = {
+        alignItems: 'center',
+        paddingStart: Size(95),
+        width: width,
+        height: height,
+        borderRadius: Size(27),
+        borderWidth: Size(1),
+        borderColor: "rgba(117, 117, 117, 255)",
+        justifyContent: 'center',
+        alignContent: 'center',
+        // marginTop: Size(66),
+        flexDirection: 'row'
+    }
+
+    let InputTextStyleCustom = {
+        width: width,
+        // height: Size(117),
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
     return (
-        <View style={styles.intputContainer} >
+        <View style={[styles.intputContainer, intputContainerCustom]} >
             {
                 propss.icon &&
                 <MaterialCommunityIcons name={propss.type ? "lock" : "rectangle"} size={24} color={colors.lightgray} />
             }
-
             <TextInput
-                style={styles.InputTextStyle}
+                style={[styles.InputTextStyle, InputTextStyleCustom]}
                 keyboardType={propss.keyType}
                 onChangeText={(text: string) => propss.onChangeText(text)}
                 value={propss.value}
@@ -55,22 +109,22 @@ export const CustomInput: React.FC<CustomInputProps> = (props) => {
 
 const styles = StyleSheet.create({
     intputContainer: {
-        alignItems: 'center',
-        paddingStart: Size(95),
-        width: wp(100) - Size(95),
-        height: Size(117),
-        borderRadius: Size(27),
-        borderWidth: Size(1),
-        borderColor: "rgba(117, 117, 117, 255)",
-        justifyContent: 'center',
-        alignContent: 'center',
-        marginTop: Size(66),
-        flexDirection: 'row'
+        // alignItems: 'center',
+        // paddingStart: Size(95),
+        // width: wp(100) - Size(95),
+        // height: Size(117),
+        // borderRadius: Size(27),
+        // borderWidth: Size(1),
+        // borderColor: "rgba(117, 117, 117, 255)",
+        // justifyContent: 'center',
+        // alignContent: 'center',
+        // marginTop: Size(66),
+        // flexDirection: 'row'
     },
     InputTextStyle: {
-        width: wp(100) - Size(95),
-        // height: Size(117),
-        justifyContent: 'center',
-        alignItems: 'center'
+        // width: wp(100) - Size(95),
+        // // height: Size(117),
+        // justifyContent: 'center',
+        // alignItems: 'center'
     }
 });
